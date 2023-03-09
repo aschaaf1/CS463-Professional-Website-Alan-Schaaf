5b799f93f089c31e6b817dce4752799e0f03a8f0
Alan <aschaaf@pdx.edu>
2023-03-09 12:25:51 -0800

    changed my navbar so that it is fixed, so it now stays fixed as the screen is scrolled. also added image of my selfie to the commit

--- Alan-Schaaf-Website.js
+++ Alan-Schaaf-Website.js
@@ -9,7 +9,16 @@ selfie.src = "images/selfie.jpg";
 selfie.alt =
   "A professional looking photograph of Alan standing in front of a white background";
 aboutMeText.textContent =
-  "Hello my name is Alan Schaaf and I am a current student at Portland State University where I am working on completing my Bachelors of Computer Science. I am in the PCEP internship program and currently working as a software engineering intern at Apex Fintech Solutions here in Portland Oregon. I  am part of the Trade Processing team at Apex. My time at Apex has so far been divided between working on many different things, from maintaining our currently deployed applications, to working on creating a brand new platform from the ground up that will be released in the future. The primary languages I have used at Apex have been Java, Go, and Javascript. I have loved my experience working as a software engineering intern, but as I near graduation I am now on the search for a full time software engineering position. Please reach out via email or phone so that we can set up an interview!";
+  "Hello my name is Alan Schaaf and I am a current student at Portland State University \
+  where I am working on completing my Bachelors of Computer Science. I am in the PCEP \
+  internship program and currently working as a software engineering intern at Apex Fintech \
+  Solutions here in Portland Oregon. I  am part of the Trade Processing team at Apex. My \
+  time at Apex has so far been divided between working on many different things, from maintaining \
+  our currently deployed applications, to working on creating a brand new platform from the ground \
+  up that will be released in the future. The primary languages I have used at Apex have been Java,\
+   Go, and Javascript. I have loved my experience working as a software engineering intern, but as\
+    I near graduation I am now on the search for a full time software engineering position. Please \
+    reach out via the contact form below so that we can set up an interview!";
 
 aboutMe.append(selfie);
 aboutMe.append(aboutMeText);
