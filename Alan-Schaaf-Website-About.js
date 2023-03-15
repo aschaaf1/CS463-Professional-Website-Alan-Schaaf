c8f2288700d647c7e36d46aad771df21a8e9798c
Alan <aschaaf@pdx.edu>
2023-03-14 19:58:55 -0700

    Made some minor changes to my about me paragraph

--- Alan-Schaaf-Website-About.js
+++ Alan-Schaaf-Website-About.js
@@ -16,10 +16,10 @@ aboutMeText.innerHTML =
   "Hello my name is Alan Schaaf and I am a current student at Portland State University \
   where I am working on completing my Bachelors of Computer Science. I am in the PCEP \
   internship program and currently working as a software engineering intern at Apex Fintech \
-  Solutions here in Portland Oregon.<br><br>At Apex I work on the Trade Processing team. Some of the technologies that I work with on a \
+  Solutions here in Portland, Oregon.<br><br>At Apex I work on the Trade Processing team.  My day to day responsiblities include working on our currently deployed trade booking API, as well as \
+  working on a brand new version two of our API that we are building from the ground up. Some of the technologies that I work with on a \
   daily basis are Kafka, Docker, Kubernetes, and Google Cloud Platform. The primary languages I use at Apex are Go, \
-  Java, and Javascript. My day to day responsiblities include maintaining our currently deployed financial trading platform, as well as \
-  working toward developing a brand new platform from the ground up.<br><br>I have loved my experience working as a software engineering intern, but as \
+  Java, and Javascript.<br><br>I have loved my experience working as a software engineering intern, but as \
   I near graduation I am now on the search for a full time software engineering position. Please \
   reach out via the contact page so that we can set up an interview!";
 aboutMe.append(aboutMeText);
