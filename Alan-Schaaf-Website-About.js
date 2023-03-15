4a97661059db7d4c0bd622b52cbc7248dc27d760
Alan <aschaaf@pdx.edu>
2023-03-14 20:02:13 -0700

    removed a word

--- Alan-Schaaf-Website-About.js
+++ Alan-Schaaf-Website-About.js
@@ -17,7 +17,7 @@ aboutMeText.innerHTML =
   where I am working on completing my Bachelors of Computer Science. I am in the PCEP \
   internship program and currently working as a software engineering intern at Apex Fintech \
   Solutions here in Portland, Oregon.<br><br>At Apex I work on the Trade Processing team.  My day to day responsiblities include working on our currently deployed trade booking API, as well as \
-  working on a brand new version two of our API that we are building from the ground up. Some of the technologies that I work with on a \
+  working on a new version two of our API that we are building from the ground up. Some of the technologies that I work with on a \
   daily basis are Kafka, Docker, Kubernetes, and Google Cloud Platform. The primary languages I use at Apex are Go, \
   Java, and Javascript.<br><br>I have loved my experience working as a software engineering intern, but as \
   I near graduation I am now on the search for a full time software engineering position. Please \
